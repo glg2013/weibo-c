68496b89c9efea0daf27c494fc191086b50a63b4
glg2013 <57412960@qq.com>
2022-01-28 10:42:01 +0000

    重置密码

--- config/app.php
+++ config/app.php
@@ -67,8 +67,8 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
-
+    //'timezone' => 'UTC',
+    'timezone' => 'PRC',
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
