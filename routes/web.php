dfaa2988758c3b2d84bb78e98b727e6c9d5f5434
glg2013 <57412960@qq.com>
2022-02-11 10:33:42 +0000

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,6 @@ Route::get('password/reset/{token}',  [PasswordController::class, 'showResetForm
 Route::post('password/reset',  [PasswordController::class, 'reset'])->name('password.update');
 
 Route::resource('statuses', StatusesController::class)->only(['store', 'destroy']);
+
+Route::get('/users/{user}/followings', [UsersController::class, 'followings'])->name('users.followings');
+Route::get('/users/{user}/followers', [UsersController::class, 'followers'])->name('users.followers');
