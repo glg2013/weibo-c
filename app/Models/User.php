d0d8aaf5accca393018b537f7078023bdfe56f13
glg2013 <57412960@qq.com>
2022-02-11 10:04:05 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,4 +69,37 @@ class User extends Authenticatable
     {
         return $this->statuses()->orderByDesc('created_at');
     }
+
+    // 粉丝
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    // 关注的人
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 }
